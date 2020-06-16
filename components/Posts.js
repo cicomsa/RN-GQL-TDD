a23a79061a415f7018aeb9be8f09247e6796951e
cicomsa <ioanacomsa85@gmail.com>
2020-06-17 00:28:18 +0100

    Posts moved to seperate file

---
+++ components/Posts.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Post from './Post'
+
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const Posts = () => {
+  const handleRemove = id => {
+    // setPosts(posts.filter(post => post.id !== id))
+    console.log('remove')
+  }
+
+  return (
+    <Query
+      query={gql`
+        {
+          posts {
+            id
+            body
+          }
+        }
+      `}
+    >
+      {({ loading, error, data }) => {
+        if (loading) return <View><Text>Loading...</Text></View>;
+        if (error) return <View><Text>Error :(</Text></View>;
+        return (
+          <FlatList
+            data={data.posts}
+            keyExtractor={item => item.id}
+            renderItem={data =>
+              <Post
+                post={data.item.body}
+                deletePost={handleRemove}
+                id={data.item.id}
+              />
+            }
+          />
+        )
+      }}
+    </Query>
+  )
+}
+
+const styles = StyleSheet.create({
+  posts: {
+    marginTop: 5
+  }
+});
+
+export default Posts
--- components/RootComponent.js
+++ components/RootComponent.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Button, Text, FlatList } from 'react-native';
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
-import Post from './Post'
+import { StyleSheet, View, Button } from 'react-native';
+
+import Posts from './Posts'
 import FlexBoxes from './FlexBoxes'
 import AddPost from './AddPost'
 
@@ -21,10 +20,6 @@ const RootComponent = () => {
     setModalIsOpen(false)
   }
 
-  const handleRemove = id => {
-    setPosts(posts.filter(post => post.id !== id))
-  }
-
   const openModal = () => setModalIsOpen(true)
   const closeModal = () => setModalIsOpen(false)
 
@@ -37,36 +32,7 @@ const RootComponent = () => {
         handleSubmit={handleSubmit}
         closeModal={closeModal}
       />
-      <Query
-        query={gql`
-      {
-        posts {
-          id
-          body
-        }
-      }
-    `}
-      >
-        {({ loading, error, data }) => {
-          console.log(loading, error, data)
-
-          if (loading) return <View><Text>Loading...</Text></View>;
-          if (error) return <View><Text>Error :(</Text></View>;
-          return (
-            <FlatList
-              data={data.posts}
-              keyExtractor={item => item.id}
-              renderItem={data =>
-                <Post
-                  post={data.item.body}
-                  deletePost={handleRemove}
-                  id={data.item.id}
-                />
-              }
-            />
-          )
-        }}
-      </Query>
+      <Posts />
     </View >
   );
 }
@@ -74,9 +40,6 @@ const RootComponent = () => {
 const styles = StyleSheet.create({
   screen: {
     padding: 30,
-  },
-  posts: {
-    marginTop: 5
   }
 });
 
